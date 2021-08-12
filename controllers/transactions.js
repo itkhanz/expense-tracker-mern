aac27d9f9aaab16431f05188a62e08eb4208f968
itkhan <itkhanz@gmail.com>
2021-08-12 17:38:07 +0200

    adding Transaction model

--- controllers/transactions.js
+++ controllers/transactions.js
@@ -1,20 +1,35 @@
+const Transaction = require("../models/Transaction");
+
 //  @desc Get all transactions
 //  @route GET /api/v1/transactions
 //  @access Public
-exports.getTransactions = (req, res, next) => {
-  res.send("GET tranactions");
+exports.getTransactions = async (req, res, next) => {
+  try {
+    const transactions = await Transaction.find();
+
+    return res.status(200).json({
+      success: true,
+      count: transactions.length,
+      data: transactions,
+    });
+  } catch (err) {
+    return res.status(500).json({
+      success: false,
+      error: "Server Error",
+    });
+  }
 };
 
 //  @desc Add transaction
 //  @route POST /api/v1/transactions
 //  @access Public
-exports.addTransaction = (req, res, next) => {
+exports.addTransaction = async (req, res, next) => {
   res.send("POST tranaction");
 };
 
 //  @desc Delete transaction
-//  @route DELETE /api/v1/transactions
+//  @route DELETE /api/v1/transactions/:id
 //  @access Public
-exports.deleteTransaction = (req, res, next) => {
+exports.deleteTransaction = async (req, res, next) => {
   res.send("DELETE tranaction");
 };
